16ccdae5afc04cf95c275bb3150e6c8effe803e7
Michael Schmidt <msrd0000@gmail.com>
2023-11-17 21:15:55 +0100

    Improved image tests (#3)

--- tests/test_Swin2SR.py
+++ tests/test_Swin2SR.py
@@ -1,7 +1,7 @@
 from spandrel import ModelLoader
 from spandrel.architectures.Swin2SR import Swin2SR
 
-from .util import ImageTestNames, ModelFile, compare_images_to_results, disallowed_props
+from .util import ModelFile, TestImage, assert_image_inference, disallowed_props
 
 
 def test_Swin2SR_4x(snapshot):
@@ -11,8 +11,8 @@ def test_Swin2SR_4x(snapshot):
     model = ModelLoader().load_from_file(file.path)
     assert model == snapshot(exclude=disallowed_props)
     assert isinstance(model.model, Swin2SR)
-    assert compare_images_to_results(
-        file.name,
-        model.model,
-        [ImageTestNames.SR_16, ImageTestNames.SR_32, ImageTestNames.SR_64],
+    assert_image_inference(
+        file,
+        model,
+        [TestImage.SR_16, TestImage.SR_32, TestImage.SR_64],
     )
