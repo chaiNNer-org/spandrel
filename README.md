8de1158635eb48f480faea92c4ec76f1551c7391
Michael Schmidt <mitchi5000.ms@googlemail.com>
2023-11-22 21:09:39 +0100

    Add Uformer architecture (#41)

--- README.md
+++ README.md
@@ -112,6 +112,7 @@ This has only been tested with the models that are linked here, and any unoffici
 #### Denoising
 
 - [SCUNet](https://github.com/cszn/SCUNet) | [GAN Model](https://github.com/cszn/KAIR/releases/download/v1.0/scunet_color_real_gan.pth) | [PSNR Model](https://github.com/cszn/KAIR/releases/download/v1.0/scunet_color_real_psnr.pth)
+- [Uformer](https://github.com/ZhendongWang6/Uformer) | [Denoise SIDD Model](https://mailustceducn-my.sharepoint.com/:u:/g/personal/zhendongwang_mail_ustc_edu_cn/Ea7hMP82A0xFlOKPlQnBJy0B9gVP-1MJL75mR4QKBMGc2w?e=iOz0zz) | [Deblur GoPro Model](https://mailustceducn-my.sharepoint.com/:u:/g/personal/zhendongwang_mail_ustc_edu_cn/EfCPoTSEKJRAshoE6EAC_3YB7oNkbLUX6AUgWSCwoJe0oA?e=jai90x)
 - [KBNet](https://github.com/zhangyi-3/KBNet) | [Models](https://mycuhk-my.sharepoint.com/personal/1155135732_link_cuhk_edu_hk/_layouts/15/onedrive.aspx?ga=1&id=%2Fpersonal%2F1155135732%5Flink%5Fcuhk%5Fedu%5Fhk%2FDocuments%2Fshare%2FKBNet%2FDenoising%2Fpretrained%5Fmodels)
 
 #### DeJPEG
---
+++ tests/test_Uformer.py
@@ -0,0 +1,37 @@
+from spandrel.architectures.Uformer import Uformer, load
+
+from .util import assert_loads_correctly
+
+
+def test_Uformer_load():
+    assert_loads_correctly(
+        load,
+        lambda: Uformer(),
+        lambda: Uformer(in_chans=1),
+        lambda: Uformer(dd_in=1),
+        # embed_dim=4 makes tests way faster
+        lambda: Uformer(embed_dim=4),
+        lambda: Uformer(embed_dim=4, depths=[2, 2, 3, 2, 2, 1, 2, 3, 2]),
+        lambda: Uformer(embed_dim=4, num_heads=[2, 3, 1, 5, 7, 7, 6, 3, 1]),
+        lambda: Uformer(embed_dim=4, win_size=9),
+        lambda: Uformer(embed_dim=4, mlp_ratio=5),
+        lambda: Uformer(embed_dim=4, qkv_bias=False, token_mlp="leff"),
+        lambda: Uformer(embed_dim=4, qkv_bias=False, token_mlp="fastleff"),
+        lambda: Uformer(embed_dim=4, qkv_bias=False, token_mlp="mlp"),
+        lambda: Uformer(embed_dim=4, token_mlp="leff"),
+        lambda: Uformer(embed_dim=4, token_mlp="fastleff"),
+        lambda: Uformer(embed_dim=4, token_mlp="mlp"),
+        lambda: Uformer(embed_dim=4, token_projection="linear"),
+        lambda: Uformer(embed_dim=4, token_projection="conv"),
+        lambda: Uformer(embed_dim=4, modulator=True),
+        lambda: Uformer(embed_dim=4, cross_modulator=True),
+        condition=lambda a, b: (
+            a.embed_dim == b.embed_dim
+            and a.mlp_ratio == b.mlp_ratio
+            and a.token_projection == b.token_projection
+            and a.win_size == b.win_size
+            and a.reso == b.reso
+            and a.dd_in == b.dd_in
+            and a.embed_dim == b.embed_dim
+        ),
+    )
