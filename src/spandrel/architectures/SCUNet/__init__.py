10a30ca21ca6d0eddda23157710426fd3ed2fa38
RunDevelopment <mitchi5000.ms@googlemail.com>
2023-11-24 13:46:05 +0100

    Remove `get_max_seq_index`

--- src/spandrel/architectures/SCUNet/__init__.py
+++ src/spandrel/architectures/SCUNet/__init__.py
@@ -3,7 +3,7 @@ from ...__helpers.model_descriptor import (
     SizeRequirements,
     StateDict,
 )
-from ..__arch_helpers.state import get_max_seq_index
+from ..__arch_helpers.state import get_seq_len
 from .arch.SCUNet import SCUNet
 
 
@@ -17,13 +17,13 @@ def load(state_dict: StateDict) -> RestorationModelDescriptor[SCUNet]:
     dim = state_dict["m_head.0.weight"].shape[0]
     in_nc = state_dict["m_head.0.weight"].shape[1]
 
-    config[0] = get_max_seq_index(state_dict, "m_down1.{}.conv1_1.weight") + 1
-    config[1] = get_max_seq_index(state_dict, "m_down2.{}.conv1_1.weight") + 1
-    config[2] = get_max_seq_index(state_dict, "m_down3.{}.conv1_1.weight") + 1
-    config[3] = get_max_seq_index(state_dict, "m_body.{}.conv1_1.weight") + 1
-    config[4] = get_max_seq_index(state_dict, "m_up3.{}.conv1_1.weight", start=1)
-    config[5] = get_max_seq_index(state_dict, "m_up2.{}.conv1_1.weight", start=1)
-    config[6] = get_max_seq_index(state_dict, "m_up1.{}.conv1_1.weight", start=1)
+    config[0] = get_seq_len(state_dict, "m_down1") - 1
+    config[1] = get_seq_len(state_dict, "m_down2") - 1
+    config[2] = get_seq_len(state_dict, "m_down3") - 1
+    config[3] = get_seq_len(state_dict, "m_body")
+    config[4] = get_seq_len(state_dict, "m_up3") - 1
+    config[5] = get_seq_len(state_dict, "m_up2") - 1
+    config[6] = get_seq_len(state_dict, "m_up1") - 1
 
     model = SCUNet(
         in_nc=in_nc,
