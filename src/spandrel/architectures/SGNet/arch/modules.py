7c0ae5284c9c115d67efc6b310a1a12f4c5b96e1
RunDevelopment <mitchi5000.ms@googlemail.com>
2024-01-03 14:47:39 +0100

    Remove scipy dep

--- src/spandrel/architectures/SGNet/arch/modules.py
+++ src/spandrel/architectures/SGNet/arch/modules.py
@@ -1,6 +1,7 @@
-import numpy as np
-import scipy.linalg
+from __future__ import annotations
+
 import torch
+import torch.linalg
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -11,26 +12,29 @@ class InvertibleConv1x1(nn.Module):
     def __init__(self, num_channels: int, LU_decomposed=False):
         super().__init__()
         w_shape = [num_channels, num_channels]
-        w_init = np.linalg.qr(np.random.randn(*w_shape))[0].astype(np.float32)
+        w_init = torch.linalg.qr(torch.randn(*w_shape))[0].float()
         if not LU_decomposed:
             # Sample a random orthogonal matrix:
-            self.register_parameter("weight", nn.Parameter(torch.Tensor(w_init)))
+            self.register_parameter("weight", nn.Parameter(w_init))
         else:
-            np_p, np_l, np_u = scipy.linalg.lu(w_init)
-            np_s = np.diag(np_u)
-            np_sign_s = np.sign(np_s)
-            np_log_s = np.log(np.abs(np_s))
-            np_u = np.triu(np_u, k=1)
-            l_mask = np.tril(np.ones(w_shape, dtype=np.float32), -1)
-            eye = np.eye(*w_shape, dtype=np.float32)
+            plu: tuple[torch.Tensor, torch.Tensor, torch.Tensor] = torch.linalg.lu(
+                w_init
+            )
+            np_p, np_l, np_u = plu
+            np_s = torch.diag(np_u)
+            np_sign_s = torch.sign(np_s)
+            np_log_s = torch.log(torch.abs(np_s))
+            np_u = torch.triu(np_u, diagonal=1)
+            l_mask = torch.tril(torch.ones(w_shape, dtype=torch.float32), -1)
+            eye = torch.eye(*w_shape, dtype=torch.float32)
 
-            self.register_buffer("p", torch.Tensor(np_p.astype(np.float32)))
-            self.register_buffer("sign_s", torch.Tensor(np_sign_s.astype(np.float32)))
-            self.l = nn.Parameter(torch.Tensor(np_l.astype(np.float32)))
-            self.log_s = nn.Parameter(torch.Tensor(np_log_s.astype(np.float32)))
-            self.u = nn.Parameter(torch.Tensor(np_u.astype(np.float32)))
-            self.l_mask = torch.Tensor(l_mask)
-            self.eye = torch.Tensor(eye)
+            self.register_buffer("p", np_p.float())
+            self.register_buffer("sign_s", (np_sign_s.float()))
+            self.l = nn.Parameter(np_l.float())
+            self.log_s = nn.Parameter(np_log_s.float())
+            self.u = nn.Parameter(np_u.float())
+            self.l_mask = l_mask
+            self.eye = eye
         self.w_shape = w_shape
         self.LU = LU_decomposed
 
@@ -49,8 +53,8 @@ class InvertibleConv1x1(nn.Module):
                 )
             return weight, dlogdet
         else:
-            self.p = self.p.to(input.device)
-            self.sign_s = self.sign_s.to(input.device)
+            self.p = self.p.to(input.device)  # type: ignore
+            self.sign_s = self.sign_s.to(input.device)  # type: ignore
             self.l_mask = self.l_mask.to(input.device)
             self.eye = self.eye.to(input.device)
             l = self.l * self.l_mask + self.eye  # noqa: E741
