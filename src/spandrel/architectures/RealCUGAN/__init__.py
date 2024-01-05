7bd7f30a80667ce65c2db74611a8ee0917cf6d7d
Michael Schmidt <mitchi5000.ms@googlemail.com>
2024-01-05 14:05:05 +0100

    Removed unnecessary CUGAN code (#117)

--- src/spandrel/architectures/RealCUGAN/__init__.py
+++ src/spandrel/architectures/RealCUGAN/__init__.py
@@ -2,8 +2,6 @@ from __future__ import annotations
 
 from typing import Literal
 
-from torch import Tensor
-
 from ...__helpers.model_descriptor import (
     ImageModelDescriptor,
     SizeRequirements,
@@ -31,32 +29,19 @@ def load(
         scale = 4
         in_channels = state_dict["unet1.conv1.conv.0.weight"].shape[1]
         out_channels = 3  # hard coded in UpCunet4x
-        model = UpCunet4x(in_channels=in_channels, out_channels=out_channels)
+        model = UpCunet4x(in_channels=in_channels, out_channels=out_channels, pro=pro)
     elif state_dict["unet1.conv_bottom.weight"].shape[2] == 5:
         # UpCunet3x
         scale = 3
         in_channels = state_dict["unet1.conv1.conv.0.weight"].shape[1]
         out_channels = state_dict["unet2.conv_bottom.weight"].shape[0]
-        model = UpCunet3x(in_channels=in_channels, out_channels=out_channels)
+        model = UpCunet3x(in_channels=in_channels, out_channels=out_channels, pro=pro)
     else:
         # UpCunet2x
         scale = 2
         in_channels = state_dict["unet1.conv1.conv.0.weight"].shape[1]
         out_channels = state_dict["unet2.conv_bottom.weight"].shape[0]
-        model = UpCunet2x(in_channels=in_channels, out_channels=out_channels)
-
-    def call_fn(model: UpCunet2x | UpCunet3x | UpCunet4x, x: Tensor) -> Tensor:
-        if pro:
-            # pro expects a different input range
-            x = x * 0.7 + 0.15
-
-        return model(
-            x,
-            tile_mode=0,  # no internal tiling
-            cache_mode=0,  # only relevant for tiling
-            alpha=1,  # adjustable parameter to control sharpness
-            pro=pro,
-        )
+        model = UpCunet2x(in_channels=in_channels, out_channels=out_channels, pro=pro)
 
     return ImageModelDescriptor(
         model,
@@ -70,5 +55,4 @@ def load(
         input_channels=in_channels,
         output_channels=out_channels,
         size_requirements=SizeRequirements(minimum=32),
-        call_fn=call_fn,
     )
