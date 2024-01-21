9ed22f1cd1425119f12855bcbac46ad906db062b
RunDevelopment <mitchi5000.ms@googlemail.com>
2024-01-21 21:59:38 +0100

    Add support for NAFNet

---
+++ tests/test_NAFNet.py
@@ -0,0 +1,54 @@
+from spandrel.architectures.NAFNet import NAFNet, load
+
+from .util import (
+    ModelFile,
+    TestImage,
+    assert_image_inference,
+    assert_loads_correctly,
+    assert_size_requirements,
+    disallowed_props,
+)
+
+
+def test_load():
+    assert_loads_correctly(
+        load,
+        lambda: NAFNet(
+            img_channel=3,
+            width=32,
+            middle_blk_num=12,
+            enc_blk_nums=[2, 2, 4, 8],
+            dec_blk_nums=[2, 2, 2, 2],
+        ),
+        lambda: NAFNet(
+            img_channel=3,
+            width=32,
+            middle_blk_num=1,
+            enc_blk_nums=[1, 1, 1, 28],
+            dec_blk_nums=[1, 1, 1, 1],
+        ),
+        condition=lambda a, b: (a.padder_size == b.padder_size),
+    )
+
+
+def test_size_requirements():
+    file = ModelFile.from_url(
+        "https://drive.google.com/file/d/1Fr2QadtDCEXg6iwWX8OzeZLbHOx2t5Bj/view",
+        name="NAFNet-GoPro-width32.pth",
+    )
+    assert_size_requirements(file.load_model())
+
+
+def test_NAFNet_GoPro_width32(snapshot):
+    file = ModelFile.from_url(
+        "https://drive.google.com/file/d/1Fr2QadtDCEXg6iwWX8OzeZLbHOx2t5Bj/view",
+        name="NAFNet-GoPro-width32.pth",
+    )
+    model = file.load_model()
+    assert model == snapshot(exclude=disallowed_props)
+    assert isinstance(model.model, NAFNet)
+    assert_image_inference(
+        file,
+        model,
+        [TestImage.BLURRY_FACE, TestImage.SR_32],
+    )
