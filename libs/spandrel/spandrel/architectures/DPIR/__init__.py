2271ca9622e61e2fc72beab7399340fad2779edb
RunDevelopment <mitchi5000.ms@googlemail.com>
2024-03-25 17:51:04 +0100

    DnCNN

--- libs/spandrel/spandrel/architectures/DPIR/__init__.py
+++ libs/spandrel/spandrel/architectures/DPIR/__init__.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from typing_extensions import override
 
-from spandrel.util import KeyCondition
+from spandrel.util import KeyCondition, get_seq_len
 
 from ...__helpers.model_descriptor import (
     Architecture,
@@ -36,6 +36,40 @@ _is_ircnn = KeyCondition.exactly(
 _is_dncnn = KeyCondition.has_all(
     "model.0.weight",
     "model.0.bias",
+    "model.2.weight",
+    "model.2.bias",
+    KeyCondition.has_any(
+        KeyCondition.has_all(
+            # act_mode="R"
+            "model.4.weight",
+            "model.4.bias",
+            "model.6.weight",
+            "model.6.bias",
+            "model.8.weight",
+            "model.8.bias",
+            "model.10.weight",
+            "model.10.bias",
+            "model.12.weight",
+            "model.12.bias",
+            "model.14.weight",
+            "model.14.bias",
+        ),
+        KeyCondition.has_all(
+            # act_mode="BR"
+            "model.3.weight",
+            "model.3.bias",
+            "model.3.running_mean",
+            "model.3.running_var",
+            "model.5.weight",
+            "model.5.bias",
+            "model.6.weight",
+            "model.6.bias",
+            "model.6.running_mean",
+            "model.6.running_var",
+            "model.8.weight",
+            "model.8.bias",
+        ),
+    ),
 )
 
 
@@ -49,9 +83,9 @@ class DPIRArch(Architecture[_DPIR]):
     @override
     def load(self, state_dict: StateDict) -> ImageModelDescriptor[_DPIR]:
         if _is_ircnn(state_dict):
-            in_nc = 1
-            out_nc = 1
-            nc = 64
+            # in_nc = 1
+            # out_nc = 1
+            # nc = 64
 
             in_nc = state_dict["model.0.weight"].shape[1]
             out_nc = state_dict["model.12.weight"].shape[0]
@@ -64,15 +98,24 @@ class DPIRArch(Architecture[_DPIR]):
                 nc=nc,
             )
         elif _is_dncnn(state_dict):
-            in_nc = 1
-            out_nc = 1
-            nc = 64
-            nb = 17
-            act_mode = "BR"
+            # in_nc = 1
+            # out_nc = 1
+            # nc = 64
+            # nb = 17
+            # act_mode = "BR"
 
             in_nc = state_dict["model.0.weight"].shape[1]
-            out_nc = state_dict["model.12.weight"].shape[0]
             nc = state_dict["model.0.weight"].shape[0]
+
+            layers = get_seq_len(state_dict, "model")
+            out_nc = state_dict[f"model.{layers-1}.weight"].shape[0]
+
+            if "model.3.weight" in state_dict:
+                act_mode = "BR"
+                nb = (layers - 3) // 3 + 2
+            else:
+                act_mode = "R"
+                nb = (layers - 3) // 2 + 2
 
             tags = ["DnCNN", f"{nc}nc", f"{nb}nb"]
             model = DnCNN(
