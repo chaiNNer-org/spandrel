e663f1b282f076cccac05e79f5fec2a67af741d9
RunDevelopment <mitchi5000.ms@googlemail.com>
2024-03-27 18:04:23 +0100

    Don't exit on invalid parameters

--- libs/spandrel/spandrel/architectures/CRAFT/arch/CRAFT.py
+++ libs/spandrel/spandrel/architectures/CRAFT/arch/CRAFT.py
@@ -141,8 +141,7 @@ class Attention_regular(nn.Module):
         elif idx == 1:
             W_sp, H_sp = self.split_size[0], self.split_size[1]
         else:
-            print("ERROR MODE", idx)
-            exit(0)
+            raise ValueError(f"ERROR MODE: {idx}")
         self.H_sp = H_sp
         self.W_sp = W_sp
 
