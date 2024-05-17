2932e801447be331e41591721ed384c2685d6d06
Joey Ballentine <joeyjballentine@gmail.com>
2024-05-17 01:30:48 -0400

    rename property to just "bytes"

--- libs/spandrel/spandrel/__helpers/model_descriptor.py
+++ libs/spandrel/spandrel/__helpers/model_descriptor.py
@@ -214,9 +214,9 @@ class ModelBase(ABC, Generic[T]):
         on how to best use the model.
         """
 
-        self.model_bytes: int = sum(p.numel() * 4 for p in model.parameters())
+        self.bytes: int = sum(p.numel() * 4 for p in model.parameters())
         """
-        The number of bytes the model uses in fp32.
+        The number of bytes the model uses in fp32 mode.
         """
 
         self.model.load_state_dict(state_dict)  # type: ignore
