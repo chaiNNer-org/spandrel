94c9abdba25cdfb632c5f2bfdbdf746b028f8781
Michael Schmidt <mitchi5000.ms@googlemail.com>
2024-07-11 14:51:23 +0200

    Include source distributions

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,11 +5,11 @@ set -e
 # spandrel
 cp README.md libs/spandrel/README.md
 cd libs/spandrel
-python3 -m build . --wheel
+python3 -m build .
 rm README.md
 cd ../..
 
 # spandrel_extra_arches
 cd libs/spandrel_extra_arches
-python3 -m build . --wheel
+python3 -m build .
 cd ../..
