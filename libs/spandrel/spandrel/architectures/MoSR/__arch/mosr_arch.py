20a82c58055a28835453b38a73d2ea153b58ff2c
the-database <25811902+the-database@users.noreply.github.com>
2024-09-10 19:22:42 -0400

    fix python 3.8 error in mosr

--- libs/spandrel/spandrel/architectures/MoSR/__arch/mosr_arch.py
+++ libs/spandrel/spandrel/architectures/MoSR/__arch/mosr_arch.py
@@ -120,7 +120,7 @@ class GatedCNNBlock(nn.Module):
 
     @staticmethod
     def _init_weights(m):
-        if isinstance(m, nn.Conv2d | nn.Linear):
+        if isinstance(m, (nn.Conv2d, nn.Linear)):
             trunc_normal_(m.weight, std=0.02)
             if m.bias is not None:
                 nn.init.constant_(m.bias, 0)
