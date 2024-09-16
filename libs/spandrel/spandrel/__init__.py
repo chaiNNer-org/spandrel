a1db3f5debbeeacbe02fb4114c69feee56ba5e21
Joey Ballentine <joeyjballentine@gmail.com>
2024-09-15 20:35:13 -0500

    Bump versions

--- libs/spandrel/spandrel/__init__.py
+++ libs/spandrel/spandrel/__init__.py
@@ -2,7 +2,7 @@
 Spandrel is a library for loading and running pre-trained PyTorch models. It automatically detects the model architecture and hyper parameters from model files, and provides a unified interface for running models.
 """
 
-__version__ = "0.3.4"
+__version__ = "0.4.0"
 
 from .__helpers.canonicalize import canonicalize_state_dict
 from .__helpers.loader import ModelLoader
--- libs/spandrel_extra_arches/spandrel_extra_arches/__init__.py
+++ libs/spandrel_extra_arches/spandrel_extra_arches/__init__.py
@@ -6,7 +6,7 @@ All architectures in this library are registered in the `EXTRA_REGISTRY` diction
 
 from .__helper import EXTRA_REGISTRY, install
 
-__version__ = "0.1.1"
+__version__ = "0.2.0"
 
 __all__ = [
     "EXTRA_REGISTRY",
