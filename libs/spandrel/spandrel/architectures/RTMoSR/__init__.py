e2386cd5028bffd65c3986d5b46b67fcf6cdbe4d
TNTwise <77559100+TNTwise@users.noreply.github.com>
2025-06-22 19:02:05 -0500

    Update __init__.py

--- libs/spandrel/spandrel/architectures/RTMoSR/__init__.py
+++ libs/spandrel/spandrel/architectures/RTMoSR/__init__.py
@@ -18,35 +18,10 @@ class RTMoSRArch(Architecture[RTMoSR]):
             id="RTMoSR",
             name="Real Time MoSR",
             detect=KeyCondition.has_all(
-                "body.0.norm.weight",
-                "body.0.norm.bias",
                 "body.0.fc1.alpha",
-                "body.0.fc1.conv1.weight",
-                "body.0.fc1.conv1.bias",
-                "body.0.fc1.conv2.weight",
-                "body.0.fc1.conv2.bias",
-                "body.0.fc1.conv3.sk.weight",
-                "body.0.fc1.conv3.sk.bias",
-                "body.0.fc1.conv3.conv.0.weight",
-                "body.0.fc1.conv3.conv.0.bias",
-                "body.0.fc1.conv3.conv.1.weight",
-                "body.0.fc1.conv3.conv.1.bias",
-                "body.0.fc1.conv3.conv.2.weight",
-                "body.0.fc1.conv3.conv.2.bias",
-                "body.0.fc1.conv3.eval_conv.weight",
-                "body.0.fc1.conv3.eval_conv.bias",
-                "body.0.fc1.conv_3x3_rep.weight",
-                "body.0.fc1.conv_3x3_rep.bias",
-                "body.0.conv.dwconv_hw.weight",
-                "body.0.conv.dwconv_hw.bias",
-                "body.0.conv.dwconv_w.weight",
-                "body.0.conv.dwconv_w.bias",
-                "body.0.conv.dwconv_h.weight",
-                "body.0.conv.dwconv_h.bias",
-                "body.0.fc2.weight",
-                "body.0.fc2.bias",
-                "to_img.0.weight",
-                "to_img.0.bias",
+                "to_img.0.alpha",
+                "to_img.0.conv1.k0",
+                "to_img.0.conv2.weight",
             ),
         )
 
@@ -54,22 +29,23 @@ class RTMoSRArch(Architecture[RTMoSR]):
     def load(self, state_dict: StateDict) -> ImageModelDescriptor[RTMoSR]:
         in_nc = 3
         out_nc = 3
-        unshuffle = False
-        if "to_feat.1.weight" in state_dict:
+        unshuffle = True
+        """if "to_feat.1.weight" in state_dict:
             unshuffle = True
             scale = math.isqrt(state_dict["to_feat.1.weight"].shape[1] // 3)
             dim = state_dict["to_feat.1.weight"].shape[0]
         else:
             scale = math.isqrt(state_dict["to_img.0.weight"].shape[0] // 3)
             dim = state_dict["to_feat.weight"].shape[0]
-        ffn = state_dict["body.0.fc1.conv1.weight"].shape[0] / dim / 2
+        ffn = state_dict["body.0.fc1.conv1.weight"].shape[0] / dim / 2"""
         n_blocks = get_seq_len(state_dict, "body")
-
+        scale = 1
+        dim=32
         model = RTMoSR(
             scale=scale,
             dim=dim,
-            ffn_expansion=ffn,
-            n_blocks=n_blocks,
+            ffn_expansion=2,
+            n_blocks=2,
             unshuffle_mod=unshuffle,
         )
 
